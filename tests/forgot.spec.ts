6e5b19696e1b9722566f18b90d24585daa43eb0d
radhakrishnan <radhakrishnan.varatharajan@twilightitsolutions.com>
2025-03-27 14:43:14 +0530

    feat: tags were implemented

--- tests/forgot.spec.ts
+++ tests/forgot.spec.ts
@@ -1,29 +1,31 @@
 
 import { test, expect } from '@playwright/test';
+import { loginCredentials, navigation } from '../utils/test-helpers';
 
 test.describe('Forgot Screen Functionality', { tag: ['@E2E'] }, () => {
   test.beforeEach(async ({ page }) => {
     // Visit the login page or main page if already logged in
-    await page.goto('https://tripvista.appxpay.in/', { waitUntil: 'load' });
+    await navigation(page);
     await expect(page.getByRole('link', { name: 'Forgot password?' })).toBeVisible();
     await page.getByRole('link', { name: 'Forgot password?' }).click();
   });
+  test.describe('UI & functionality validation', { tag: ['@forgot'] }, () => {
+    test('TC_FORGOTPASSWORD_09 : Verify "Forgot Password" functionality', { tag: ['@smoke'] }, async ({ page }) => {
+      await expect(page.getByRole('textbox', { name: 'Enter your email' })).toBeVisible();
+      await expect(page.getByRole('button', { name: 'Submit' })).toBeVisible();
+    });
 
-  test('TC_FORGOTPASSWORD_09 : Verify "Forgot Password" functionality', async ({ page }) => {
-    await expect(page.getByRole('textbox', { name: 'Enter your email' })).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Submit' })).toBeVisible();
+    test('TC_FORGOTPASSWORD_10 : Ensure that a valid toast message is displayed when the system is unable to identify the user, stating: "Unable to identify the User."', { tag: ['@smoke'] }, async ({ page }) => {
+      await expect(page.getByRole('textbox', { name: 'Enter your email' })).toBeVisible();
+      await page.getByRole('textbox', { name: 'Enter your email' }).fill('ggdf');
+      await expect(page.getByText('Please enter a valid email')).toBeVisible();
+    });
+
+    test('TC_FORGOTPASSWORD_11 : Ensure that a valid toast message is displayed when the system is unable to identify the user, stating: "successfully sent the Email""', { tag: ['@smoke'] }, async ({ page }) => {
+      await expect(page.getByRole('textbox', { name: 'Enter your email' })).toBeVisible();
+      await page.getByRole('textbox', { name: 'Enter your email' }).fill('agent@gmail.com');
+      await page.getByRole('button', { name: 'Submit' }).click();
+    });
+
   });
-
-  test('TC_FORGOTPASSWORD_10 : Ensure that a valid toast message is displayed when the system is unable to identify the user, stating: "Unable to identify the User."', async ({ page }) => {
-    await expect(page.getByRole('textbox', { name: 'Enter your email' })).toBeVisible();
-    await page.getByRole('textbox', { name: 'Enter your email' }).fill('ggdf');
-    await expect(page.getByText('Please enter a valid email')).toBeVisible();
-  });
-
-  test('TC_FORGOTPASSWORD_11 : Ensure that a valid toast message is displayed when the system is unable to identify the user, stating: "successfully sent the Email""', async ({ page }) => {
-    await expect(page.getByRole('textbox', { name: 'Enter your email' })).toBeVisible();
-    await page.getByRole('textbox', { name: 'Enter your email' }).fill('agent@gmail.com');
-    await page.getByRole('button', { name: 'Submit' }).click();
-  });
-
 });
--- tests/logout.spec.ts
+++ tests/logout.spec.ts
@@ -1,29 +1,30 @@
 import { test, expect } from '@playwright/test';
 import { loginCredentials, navigation } from '../utils/test-helpers';
 
-test.describe.only('Logout Screen Functionality', { tag: ['@E2E'] }, () => {
+test.describe('Logout Screen Functionality', { tag: ['@E2E'] }, () => {
 
     test.beforeEach(async ({ page }) => {
         await navigation(page);
     });
-    test('TC_LOGOUT_13 : Verify Logout Functionality', async ({ page }) => {
-        await loginCredentials(page, 'agent@gmail.com', 'SmartWork@1234');
 
-        await expect(page.getByText('Login Successful! Welcome')).toBeVisible();
-        await expect(page.getByRole('link', { name: 'Trip Vista' })).toBeVisible();
+    test.describe('UI &  functionality validation', { tag: ['@logout'] }, () => {
 
-        const myBalanceLocator = page.locator('div').filter({ hasText: /^MyBalance : ₹[\d,]+\.\d{2}$/ }).nth(2);
-        await expect(myBalanceLocator).toBeVisible();
+        test('TC_LOGOUT_13 : Verify Logout Functionality', { tag: ['@smoke'] }, async ({ page }) => {
+            await loginCredentials(page, 'agent@gmail.com', 'SmartWork@1234');
 
-        const balanceText = await myBalanceLocator.textContent();
-        console.log('Displayed Balance: ', balanceText);
+            await expect(page.getByText('Login Successful! Welcome')).toBeVisible();
+            await expect(page.getByRole('link', { name: 'Trip Vista' })).toBeVisible();
 
-        await expect(page.getByRole('img', { name: '@simonguo' })).toBeVisible();
-        await page.getByText('Sign out').click();
-        await expect(page.getByText('Logged out Successfully...')).toBeVisible();
+            const myBalanceLocator = page.locator('div').filter({ hasText: /^MyBalance : ₹[\d,]+\.\d{2}$/ }).nth(2);
+            await expect(myBalanceLocator).toBeVisible();
 
+            const balanceText = await myBalanceLocator.textContent();
+            console.log('Displayed Balance: ', balanceText);
+
+            await expect(page.getByRole('img', { name: '@simonguo' })).toBeVisible();
+            await page.getByText('Sign out').click();
+            await expect(page.getByText('Logged out Successfully...')).toBeVisible();
+        });
 
     });
-
-
 });
